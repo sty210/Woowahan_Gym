229fc5488530d0debb81d5b039b18f11117043fe
TaeYang Song <tysong@woowahan.com>
2016-01-12 17:11:20 +0900

    migrate파일 중 admins에 끼어 있던 이상한 줄 제거..

--- db/migrate/20160110152721_create_admins.rb
+++ db/migrate/20160110152721_create_admins.rb
@@ -1,9 +1,5 @@
 class CreateAdmins < ActiveRecord::Migration[5.0]
   def change
-<<<<<<< HEAD
-=======
-    # drop_table :admins
->>>>>>> d7e83b8befe27844801ac049d523dd392e8bd944
     create_table :admins do |t|
       t.string :adm_id
       t.string :adm_pw
