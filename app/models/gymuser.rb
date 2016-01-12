9c1b48a889480348d4bc540373781561bcfd6abf
TaeYang Song <tysong@woowahan.com>
2016-01-12 16:26:31 +0900

    travis fail에 따른 migrate 수행 travis의 The command "bundle exec rake db:migrate RAILS_ENV=test" exited with 1. 에러해결하려고 시도한 후 commit jira task : http://jira.woowa.in/browse/ENGITRNA-49

--- app/models/gymuser.rb
+++ app/models/gymuser.rb
@@ -1,6 +1,6 @@
 class Gymuser < ApplicationRecord
-  belongs_to :gym, class_name: 'Gym', foreign_key: 'id'
-  belongs_to :user, class_name: 'User', foreign_key: 'id'
+  belongs_to :gym
+  belongs_to :user
 
   validates :gym_no, presence: true
   validates :user_no, presence: true
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
-  belongs_to :gym, class_name: 'Gym', foreign_key: 'id'
-  belongs_to :user, class_name: 'User', foreign_key: 'id'
+  belongs_to :gym
+  belongs_to :user
 
   validates :gym_no, presence: true
   validates :rv_wr_id, presence: true, length: { minimum: 6 }
